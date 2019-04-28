189d6da305d35b5a08487321ae7fbd3364ff5de3
本田英太郎 <hondaeitaro@oolongtea.local>
2019-04-28 17:54:46 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -9,11 +9,11 @@ $(function(){
                   ${message.image}
                 </p>`
     return html;
-  }
+  };
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action')
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
@@ -21,16 +21,16 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
-    })
+    });
     .done(function(data){
       var html = buildHTML(data);
-      $('.message').append(html)
-      $('.input-textarea').val('')
-      $('.hidden')
-      $("form")[0].reset()
-    })
+      $('.message').append(html);
+      $('.input-textarea').val('');
+      $('#new_message')[0].reset()
+      // $("#new_message").prop("disabled", false);
+    });
     .fail(function(){
       alert('error');
-    })
-  })
+    });
+  });
 }); 
