fc7263561165be9dc9fd6bf8b8d9569e3d2796ae
本田英太郎 <hondaeitaro@oolongtea.local>
2019-04-30 14:31:58 +0900

    Update index.js

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -42,17 +42,17 @@ $(function() {
       })
 
 
-    .done(function(users) {
-      $("#user_search_result").empty();
-        if (users.length !== 0) {
-          users.forEach(function(user){
-            appendUsers(user);
-          })
-        }
+  .done(function(users) {
+    $("#user_search_result").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendUsers(user);
         })
-    .fail(function() {
-      alert('ユーザー検索に失敗しました');
-    });
+       }
+      })
+      .fail(function() {
+        alert('ユーザー検索に失敗しました');
+      });
     });
   });
 
