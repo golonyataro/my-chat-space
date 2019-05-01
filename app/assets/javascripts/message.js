b716f85e0f3c91099b9559888fa00629804bd5d9
本田英太郎 <hondaeitaro@oolongtea.local>
2019-05-01 23:37:41 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,16 +1,29 @@
 $(function(){
   function messagebuildHTML(message){
-    var html = `<p>
-                  <strong>
-                    <a href=/groups/${message.group_id}/messages</a>
-                    ：
-                  </strong>
-                  ${message.content}
-                  ${message.image}
-                </p>`
+    var insertImage = '';
+    if (message.image) {
+      insertImage = `<img class="image, src="${message.image}">`;
+    }
+    var html = `
+    <div class='message' data-id="${message.id}">
+      <div class='upper-info'>
+        <p class='upper-info-user'>
+          ${message.name}
+        </p>
+        <p class='upper-info-date'>
+          ${message.created_at}
+        </p>
+      </div>
+      <div class='lower-info'>
+        <p class='message-text'>
+          ${message.content}
+        </p>
+      </div>
+    </div> `
     return html;
   };
   
+  //非同期通信
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -22,8 +35,7 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
-    });
-
+    })
     .done(function(message) {
       var html = messagebuildHTML(message);
       $('.message').append(html)
@@ -33,4 +45,32 @@ $(function(){
       alert('error');
     });
   });
+
+  //自動更新
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data('id');
+    console.log(last_message_id);
+    $.ajax({
+      url: location.href.json,
+      type: 'get',
+      dataType: 'json',
+      data: {last_message_id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log(last_message_id);
+      console.log(messages);
+      var insertHTML = '';
+      messages.forEach(function(message) {
+        console.log("if文");
+        if (message.id > last_message_id ) {
+          insertHTML += messagebuildHTML(message);
+        }
+      });
+      $('.messages').append(insertHTML);
+    })
+    .fail(function() {
+      alert('自動更新に失敗しました');
+    });
+  };
+  setInterval(reloadMessages, 5000);
 }); 
